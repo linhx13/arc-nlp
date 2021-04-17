53c0ea72387faf22ec2e9fa8bc48a75a4f1cf7e0
linhx13 <mylhx288@gmail.com>
2021-04-17 19:12:59 +0800

    Use pl in pytorch text classification example

--- examples/torch/text_classification/train.py
+++ examples/torch/text_classification/train.py
@@ -4,7 +4,6 @@ import time
 from functools import partial
 
 import jieba
-import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -19,7 +18,8 @@ from torchtext.experimental.functional import (
 from torchtext.vocab import Vocab, build_vocab_from_iterator, Vectors
 
 from arcnlp.torch.models import *
-from arcnlp.torch.nn import BOWEncoder
+
+import pytorch_lightning as pl
 
 jieba.initialize()
 
@@ -28,7 +28,12 @@ train_path = os.path.expanduser(os.path.join(DATA_DIR, "train.txt"))
 valid_path = os.path.expanduser(os.path.join(DATA_DIR, "dev.txt"))
 class_path = os.path.expanduser(os.path.join(DATA_DIR, "class.txt"))
 
-embedding_path = "/mnt/8f00be1b-84a6-4b38-8b59-47075910175b/datasets/sgns.sogounews.bigram-char"
+EMBEDDING_PATH = "/mnt/8f00be1b-84a6-4b38-8b59-47075910175b/datasets/sgns.sogounews.bigram-char"
+
+MAX_EPOCHS = 10
+BATCH_SIZE = 128
+
+ARCH = "TextRCNN"
 
 
 def tokenizer(text):
@@ -57,33 +62,27 @@ def build_dataset(data_path, vocab):
     return dataset
 
 
-train_dataset = build_dataset(train_path, None)
-vocab = train_dataset.get_vocab()
-valid_dataset = build_dataset(valid_path, vocab)
-
-with open(class_path) as fin:
-    class_list = [line.strip() for line in fin]
-
-print(len(train_dataset))
-print(len(valid_dataset))
-print(len(vocab), len(class_list))
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-padding_idx = vocab["<pad>"]
-print("padding_idx", padding_idx)
+def load_data(train_path, valid_path, class_path):
+    train_dataset = build_dataset(train_path, vocab=None)
+    valid_dataset = build_dataset(valid_path, vocab=train_dataset.get_vocab())
+    with open(class_path) as fin:
+        class_list = [line.strip() for line in fin]
+    return train_dataset, valid_dataset, class_list
 
 
-vectors = Vectors(embedding_path)
-vocab.load_vectors(vectors)
-
-# embedding = nn.Embedding(len(vocab), 300, padding_idx=padding_idx)
-embedding = nn.Embedding.from_pretrained(
-    vocab.vectors, freeze=False, padding_idx=padding_idx
-)
-
-
-def build_model(arch, num_classes, embedding, padding_idx):
+def build_model(arch, num_classes, vocab, embedding_path=None):
+    padding_idx = vocab["<pad>"]
+    if embedding_path is not None:
+        vectors = Vectors(embedding_path)
+        vocab.load_vectors(vectors)
+        embedding = nn.Embedding.from_pretrained(
+            vocab.vectors, freeze=False, padding_idx=padding_idx
+        )
+    else:
+        embedding = nn.Embedding(len(vocab), 300, padding_idx=padding_idx)
     if arch == "BOW":
+        from arcnlp.torch.nn import BOWEncoder
+
         return TextClassifier(
             num_classes,
             embedding,
@@ -96,12 +95,6 @@ def build_model(arch, num_classes, embedding, padding_idx):
         return TextRNN(num_classes, embedding, padding_idx=padding_idx)
     elif arch == "TextRCNN":
         return TextRCNN(num_classes, embedding, padding_idx=padding_idx)
-
-
-# arch = "BOW"
-# arch = "TextCNN"
-arch = "TextRNN"
-model = build_model(arch, len(class_list), embedding, padding_idx)
 
 
 def collate_fn(batch, padding_idx, max_len=None):
@@ -119,92 +112,111 @@ def collate_fn(batch, padding_idx, max_len=None):
     return padded_texts, labels
 
 
-def train_func(train_data, model, criterion, optimizer, scheduler, device):
-    model.train()
-    train_loss = 0
-    train_acc = 0
-    for texts, labels in train_data:
-        optimizer.zero_grad()
-        texts, labels = texts.to(device), labels.to(device)
-        preds = model(texts)
-        loss = criterion(preds, labels)
-        train_loss += loss.item()
-        loss.backward()
-        optimizer.step()
-        train_acc += (preds.argmax(1) == labels).sum().item()
-
-    if scheduler is not None:
-        scheduler.step()
-    return train_loss / len(train_data.dataset), train_acc / len(
-        train_data.dataset
+def build_dataloader(dataset, batch_size, shuffle, padding_idx):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        collate_fn=partial(collate_fn, padding_idx=padding_idx),
+        num_workers=4,
     )
 
 
-def test_func(test_data, model, criterion, device):
-    model.eval()
-    loss = 0
-    acc = 0
-    for texts, labels in test_data:
-        texts, labels = texts.to(device), labels.to(device)
-        with torch.no_grad():
-            preds = model(texts)
-            loss = criterion(preds, labels)
-            loss += loss.item()
-            acc += (preds.argmax(1) == labels).sum().item()
+class Task(pl.LightningModule):
+    def __init__(self, model):
+        super(Task, self).__init__()
+        self.model = model
+        self.criterion = nn.CrossEntropyLoss()
+        self.train_accuracy = pl.metrics.Accuracy()
+        self.valid_accuracy = pl.metrics.Accuracy()
 
-    return loss / len(test_data.dataset), acc / len(test_data.dataset)
+    def forward(self, tokens):
+        return self.model(tokens)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+
+    def step(self, batch, batch_idx):
+        tokens, labels = batch
+        outputs = self(tokens)
+        loss = self.criterion(outputs, labels.long())
+        preds = torch.argmax(outputs, axis=1)
+        return {"loss": loss, "preds": preds}
+
+    def training_step(self, batch, batch_idx):
+        res = self.step(batch, batch_idx)
+        acc = self.train_accuracy(res["preds"], batch[1])
+        self.log(
+            "train_loss",
+            res["loss"],
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=False,
+        )
+        self.log(
+            "train_acc",
+            acc,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=False,
+        )
+        return res["loss"]
+
+    def validation_step(self, batch, batch_idx):
+        res = self.step(batch, batch_idx)
+        acc = self.valid_accuracy(res["preds"], batch[1])
+        self.log(
+            "val_loss",
+            res["loss"],
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=False,
+        )
+        self.log(
+            "val_acc",
+            acc,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            logger=False,
+        )
 
 
-criterion = torch.nn.CrossEntropyLoss().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
-
-
-N_EPOCHS = 10
-BATCH_SIZE = 128
-min_valid_loss = float("inf")
-
-train_dataloader = DataLoader(
-    train_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=True,
-    collate_fn=partial(collate_fn, padding_idx=padding_idx),
-    num_workers=4,
-)
-valid_dataloader = DataLoader(
-    valid_dataset,
-    batch_size=BATCH_SIZE,
-    shuffle=False,
-    collate_fn=partial(collate_fn, padding_idx=padding_idx),
-    num_workers=4,
-)
-
-model = model.to(device)
-for epoch in range(N_EPOCHS):
-    start_time = time.time()
-    train_loss, train_acc = train_func(
-        train_dataloader, model, criterion, optimizer, scheduler, device
+def run():
+    train_dataset, valid_dataset, class_list = load_data(
+        train_path, valid_path, class_path
     )
-    valid_loss, valid_acc = test_func(
-        valid_dataloader, model, criterion, device
+    vocab = train_dataset.get_vocab()
+    train_dataloader = build_dataloader(
+        train_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+        padding_idx=vocab["<pad>"],
     )
-
-    secs = int(time.time() - start_time)
-    mins = secs / 60
-    secs = secs % 60
-
-    print(
-        "Epoch: %d" % (epoch + 1),
-        " | time in %d minutes, %d seconds" % (mins, secs),
+    valid_dataloader = build_dataloader(
+        valid_dataset,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        padding_idx=vocab["<pad>"],
     )
-    print(
-        f"\tLoss: {train_loss:.4f}(train)\t|\tAcc: {train_acc * 100:.1f}%(train)"
+    model = build_model(ARCH, len(class_list), vocab, EMBEDDING_PATH)
+    task = Task(model)
+    early_stopping = pl.callbacks.EarlyStopping("val_loss", patience=3)
+    trainer = pl.Trainer(
+        gpus=-1 if torch.cuda.is_available() else 0,
+        max_epochs=MAX_EPOCHS,
+        callbacks=[early_stopping],
+        logger=False,
+        checkpoint_callback=False,
     )
-    print(
-        f"\tLoss: {valid_loss:.4f}(valid)\t|\tAcc: {valid_acc * 100:.1f}%(valid)"
-    )
+    trainer.fit(task, train_dataloader, valid_dataloader)
 
 
-print("Checking the results of valid dataset...")
-valid_loss, valid_acc = test_func(valid_dataloader, model, criterion, device)
-print(f"\tLoss: {valid_loss:.4f}(test)\t|\tAcc: {valid_acc * 100:.1f}%(test)")
+if __name__ == "__main__":
+    pl.seed_everything(42)
+    run()
